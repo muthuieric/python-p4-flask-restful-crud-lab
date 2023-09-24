be2a064809f068f3ee145c00d05a1b6a20ef2c51
Eric <ericmuthuipatch22@gmail.com>
2023-09-24 22:49:23 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -46,9 +46,43 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+
+    def patch(self, id):
+
+        data = request.get_json()
+
+        plant = Plant.query.filter_by(id=id).first()
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict = plant.to_dict()
+
+        response = make_response(
+            jsonify(plant_dict),
+            200
+        )
+
+        return response
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if not plant:
+            return make_response(jsonify({'error': 'Plant not found'}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return '', 204 
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
+
+
 
 
 if __name__ == '__main__':
